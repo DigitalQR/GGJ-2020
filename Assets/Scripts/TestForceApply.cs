5fe03226afdabb919303fc94a973fcb5f64f19d1
Sam Potter <sampot97@gmail.com>
2020-02-01 13:40:29 +0000

    Testing with forces

---
+++ Assets/Scripts/TestForceApply.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestForceApply : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 m_Force;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (TryGetComponent(out Rigidbody body))
+		{
+			body.AddForceAtPosition(m_Force, body.centerOfMass);
+		}
+    }
+}
